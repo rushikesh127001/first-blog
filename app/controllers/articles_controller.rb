8c68c97e43b24c81b0652c53b7ee7b7b0ae76574
Rushikesh127001 <chasrushikesh@gmail.com>
2019-08-01 05:14:49 +0530

    completed all crud ops

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+    
+    def index
+        @articles=Article.all
+    end
     def new
         @article=Article.new
 
@@ -18,7 +22,31 @@ class ArticlesController < ApplicationController
     def show 
         @article=Article.find(params[:id])
 
+    end  
+
+    def edit
+        @article=Article.find(params[:id])
     end    
+    
+    def update
+        @article=Article.find(params[:id])
+
+        if @article.update(article_params)
+            flash[:notice]="edited successfully"
+            redirect_to article_path(@article)
+        else
+            render :edit
+        end        
+
+    end   
+
+    def destroy
+        @article=Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
+
+    end
+    
     
     private
         def article_params
