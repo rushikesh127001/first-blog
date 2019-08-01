641f5ec769b5646aae252a3c3041b3754f4f2319
Rushikesh127001 <chasrushikesh@gmail.com>
2019-08-01 23:45:28 +0530

    added users

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
     
+  http_basic_authenticate_with name: "rushikesh", password: "pass", except: [:index, :show]
+    
     def index
         @articles=Article.all
     end
