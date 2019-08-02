888e88a5c31047365d304b97dcb9fea26fc920ec
Rushikesh127001 <chasrushikesh@gmail.com>
2019-08-02 19:03:56 +0530

    added pagination

--- Gemfile
+++ Gemfile
@@ -9,6 +9,8 @@ gem 'rails', '~> 5.2.3'
 gem 'bootstrap-sass', '~> 3.4.1'
 gem 'jquery-rails'
 gem 'bcrypt', '~> 3.1.7' 
+gem 'will_paginate', '~>3.1.5'
+gem 'bootstrap-will_paginate', '~>1.0.0'
 
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
     def new
         @user=User.new
     end  
+
+    def index
+        @users=User.paginate(page: params[:page], per_page: 2)
+    end    
     
     def create
         @user=User.new(user_params)
@@ -33,6 +37,7 @@ class UsersController < ApplicationController
 
     def show
         @user=User.find(params[:id])
+        @user_articles=@user.articles.paginate(page: params[:page], per_page: 5)
     end    
     
     private 
