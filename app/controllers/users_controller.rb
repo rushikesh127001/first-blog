2ad79724e5d6237ac66fde0c31051cfb6ddd5745
Rushikesh127001 <chasrushikesh@gmail.com>
2019-08-03 00:11:51 +0530

    added user login and logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,7 +37,7 @@ class UsersController < ApplicationController
 
     def show
         @user=User.find(params[:id])
-        @user_articles=@user.articles.paginate(page: params[:page], per_page: 5)
+        @user_articles=@user.articles.paginate(page: params[:page], per_page: 2)
     end    
     
     private 
